5896370489970f65938615fc8b8438a292392d8a
SkyLightQP <combbm@naver.com>
2018-10-20 14:19:24 +0900

    폰트, 폰트 라이센스 업로드

--- layouts/footer.vue
+++ layouts/footer.vue
@@ -1,13 +1,17 @@
 <template>
-    <footer class="footer">
-        <div class="content has-text-centered">
-            <p>
-                Copyright 2018. SkyLightQP(하늘빛QP, 하늘빛). All rights reserved. <br>
-                문의: admin@skylightqp.kr <br>
-                [ 폰트 라이선스 첨부 ]
-            </p>
-        </div>
-    </footer>
+    <section>
+        <br>
+        <footer class="footer">
+            <div class="content has-text-centered">
+                <p>
+                    Copyright 2018. SkyLightQP(하늘빛QP, 하늘빛). All rights reserved. <br>
+                    문의: admin@skylightqp.kr <br>
+                    이 홈페이지는 모바일 지원을 하고 있지 않습니다.
+                </p>
+                <a href="./FONT_LICENSE.txt">이 홈페이지는 'Google Noto Sans', '배달의 민족 주아체'를 사용 및 포함하고 있습니다.</a>
+            </div>
+        </footer>
+    </section>
 </template>
 
 <script>
