685991cbeb7e1eb51430d54488abe8b6a51c3437
SkyLightQP <combbm@naver.com>
2018-11-19 19:41:20 +0900

    연락처 레이아웃 변경

--- layouts/contacts.vue
+++ layouts/contacts.vue
@@ -1,16 +1,10 @@
 <template>
     <section>
         <h1 class="title is-4" style="font-weight: 500;">연락처</h1>
-        <div class="columns">
-            <div class="column">
-                <a class="button is-info is-outlined" href="https://t.me/SkyLightQP"><i class="fab fa-telegram"></i>&nbsp;Telegram @SkyLightQP</a>
-            </div>
-            <div class="column">
-                <a class="button is-link is-outlined" href="https://discordapp.com/users/229015522484682752"><i class="fab fa-discord"></i>&nbsp;Discord 하늘빛QP#5778</a>
-            </div>
-            <div class="column">
-                <a class="button is-primary is-outlined" href="mailto://me@skylightqp.kr"><i class="fas fa-envelope"></i>&nbsp;Email me@skylightqp.kr</a>
-            </div>
+        <div class="buttons has-addons">
+            <a class="button is-info is-outlined" href="https://t.me/SkyLightQP"><i class="fab fa-telegram"></i>&nbsp;Telegram @SkyLightQP</a>
+            <a class="button is-link is-outlined" href="https://discordapp.com/users/229015522484682752"><i class="fab fa-discord"></i>&nbsp;Discord 하늘빛QP#5778</a>
+            <a class="button is-primary is-outlined" href="mailto://me@skylightqp.kr"><i class="fas fa-envelope"></i>&nbsp;Email me@skylightqp.kr</a>
         </div>
     </section>
 </template>
